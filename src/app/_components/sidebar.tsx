4fd6adeffa417e3aaca214f9d672914d0a770433
longj724 <justinlong596@gmail.com>
2024-03-15 23:34:04 -0400

    Update sidebar

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -1,6 +1,7 @@
 // External Dependencies
 import { Folder } from "lucide-react";
 import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 
 // Relative Dependencies
 import { Group, Project, SidebarItem as SidebarItemType } from "~/lib/types";
@@ -30,37 +31,39 @@ const Sidebar = async () => {
     }),
   );
 
-  const sidebarItems: SidebarItemType[] = groupsWithProjects.map((group) => {
-    return {
-      title: group.name,
-      path: `/repositories/${group.path}`,
-      icon: <Folder width="24" height="24" />,
-      submenu: true,
-      subMenuItems: group.projects.map((project: Project) => {
-        return {
-          title: project.name,
-          path: `/repositories/${group.path}/${project.id}`,
-          icon: <Folder width="24" height="24" />,
-        };
-      }),
-    };
-  });
+  const sidebarItems: SidebarItemType[] = groupsWithProjects.map((group) => ({
+    title: group.name,
+    path: `/repositories/${group.path}`,
+    icon: <Folder width="24" height="24" />,
+    submenu: true,
+    subMenuItems: group.projects.map((project: Project) => {
+      return {
+        title: project.name,
+        path: `/repositories/${group.path}/${project.id}`,
+        icon: <Folder width="24" height="24" />,
+      };
+    }),
+  }));
 
   return (
     <div className="fixed hidden h-screen flex-1 border-r border-zinc-200 bg-white md:flex md:w-60">
-      <div className="flex w-full flex-col space-y-6">
-        <Link
-          href="/"
-          className="flex h-12 w-full flex-row items-center justify-center space-x-3 border-b border-zinc-200 md:justify-start md:px-6"
-        >
-          <span className="h-7 w-7 rounded-lg bg-zinc-300" />
-          <span className="hidden text-xl font-bold md:flex">Logo</span>
-        </Link>
-
-        <div className="flex flex-col space-y-2  md:px-6 ">
-          {sidebarItems.map((item, idx) => {
-            return <SidebarItem key={idx} item={item} />;
-          })}
+      <div className="flex w-full flex-col ">
+        <div className="flex flex-row items-center">
+          <Link
+            href="/"
+            className="ml-4 flex h-12 w-full flex-row items-center justify-center  space-x-3 md:justify-start"
+          >
+            <p className="hidden text-xl font-bold md:flex">AI Code Review</p>
+          </Link>
+          <div className="ml-auto mr-6">
+            <UserButton />
+          </div>
+        </div>
+        <hr className="h-[1px] w-full bg-gray-200" />
+        <div className="mt-4 flex flex-col  space-y-2 md:px-6">
+          {sidebarItems.map((item, idx) => (
+            <SidebarItem key={idx} item={item} />
+          ))}
         </div>
       </div>
     </div>
